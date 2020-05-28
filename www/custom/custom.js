f8ba9fa43d344a2405964d3a94e7460e2d96c157
Rolo <rodri.lopez98@gmail.com>
2020-05-28 01:05:10 -0300

    Added basic flow, camera functional

--- www/custom/custom.js
+++ www/custom/custom.js
@@ -4,16 +4,18 @@ function deviceReady() {
 	try {
 		//Example when Internet connection is needed but not mandatory
 		//Ejemplo de cuando se necesita conexióna a Internet pero no es obligatoria.
-		if (!mui.connectionAvailable()){
+		if (!mui.connectionAvailable()) {
 			if ('plugins' in window && 'toast' in window.plugins)
 				mui.toast('We recommend you connect your device to the Internet');
 			else
 				mui.alert('We recommend you connect your device to the Internet');
 		}
-		
+
 		//Install events, clicks, resize, online/offline, etc. 
 		installEvents();	//Events installation using MobileUI's method.
 		//isntallEvents2();	//Example of traditional events installation.
+
+		prepareCamera();
 
 		//Hide splash.
 		//Ocultar el splash.
@@ -31,8 +33,41 @@ function deviceReady() {
  * Example of Install events using MobileUI methods, clicks, resize, online/offline, etc., on differents HTML elements.
  * Ejemplo de instalación de eventos usando métodos de MobileUI, clicks, resize, online/offline, etc., sobre diferentes elementos HTML.
  */
+
+function prepareCamera() {
+	// Set constraints for the video stream
+	let constraints = { video: { facingMode: "user" }, audio: false };
+	// Define constants
+	const cameraView = document.querySelector("#camera-view"),
+		cameraOutput = document.querySelector("#camera-output"),
+		cameraSensor = document.querySelector("#camera-sensor"),
+		cameraTrigger = document.querySelector("#camera-trigger")
+	// Access the device camera and stream to cameraView
+	function cameraStart() {
+		navigator.mediaDevices
+			.getUserMedia(constraints)
+			.then(function (stream) {
+				track = stream.getTracks()[0];
+				cameraView.srcObject = stream;
+			})
+			.catch(function (error) {
+				console.error("Oops. Something is broken.", error);
+			});
+	}
+	// Take a picture when cameraTrigger is tapped
+	cameraTrigger.onclick = function () {
+		cameraSensor.width = cameraView.videoWidth;
+		cameraSensor.height = cameraView.videoHeight;
+		cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+		cameraOutput.src = cameraSensor.toDataURL("image/webp");
+		cameraOutput.classList.add("taken");
+	};
+	// Start the video stream when the window loads
+	window.addEventListener("load", cameraStart, false);
+}
+
 function installEvents() {
-	
+
 	mui.util.installEvents([
 		//Mail list click/touch events. See that if the event is not specified, click is assumed.
 		{
@@ -52,64 +87,45 @@ function installEvents() {
 			}
 		},
 		{
-			id: '#delete-me',
+			id: '#home-button',
 			ev: 'click',	//If not, it assumes click
 			fn: () => {
-				mui.viewport.showPage("template-page", "DEF");
+				mui.viewport.showPage("home-page", "DEF");
 				return false;
 			}
 		},
 		{
-			id: '#option1',
+			id: '#search-button',
+			ev: 'click',
 			fn: () => {
-				mui.screen.closePanel(function() {
-					mui.viewport.showPage("home-page", "DEF");
+				mui.viewport.showPage("search-page", "DEF");
+				return false;
+			}
+		},
+		{
+			id: '#camera-button',
+			fn: () => {
+				mui.screen.closePanel(function () {
+					mui.viewport.showPage("camera-page", "DEF");
 				});
 				return false;
 			}
 		},
 		{
-			id: '#option2',
+			id: '#hanger-button',
 			fn: () => {
-				mui.screen.closePanel(function() {
-					mui.viewport.showPage("template-page", "DEF");
+				mui.screen.closePanel(function () {
+					mui.viewport.showPage("hanger-page", "DEF");
 				});
 				return false;
 			}
 		},
-		//Toolbar options ------------------------------------------
 		{
-			id: '#tabbar-button1',
+			id: '#social-button',
 			fn: () => {
-				mui.alert("tab 1","Selected");
-				return false;
-			}
-		},
-		{
-			id: '#tabbar-button2',
-			fn: () => {
-				mui.alert("tab 2","Selected");
-				return false;
-			}
-		},
-		{
-			id: '#tabbar-button3',
-			fn: () => {
-				mui.alert("tab 3","Selected");
-				return false;
-			}
-		},
-		{
-			id: '#tabbar-button4',
-			fn: () => {
-				mui.alert("tab 4","Selected");
-				return false;
-			}
-		},
-		{
-			id: '#tabbar-button5',
-			fn: () => {
-				mui.alert("tab 5","Selected");
+				mui.screen.closePanel(function () {
+					mui.viewport.showPage("social-page", "DEF");
+				});
 				return false;
 			}
 		},
@@ -174,57 +190,57 @@ function installEvents2() {
 
 	//It's a good idea to consider what happens when the device is switched on and off the internet.
 	//Es buena idea considerar que pasa cuando el dispositivo se conecta y desconecta a Internet.
-	document.addEventListener("online", function() {
+	document.addEventListener("online", function () {
 		//somthing
 	}, false);
-	
+
 	//Back button.
-	$(".mui-backarrow").click(function() {
+	$(".mui-backarrow").click(function () {
 		mui.history.back();
 		return false;
 	});
-	
+
 	//Open menu.
-	$(".mui-headmenu").click(function() {
+	$(".mui-headmenu").click(function () {
 		mui.screen.showPanel("menu-panel", "SLIDE_LEFT");	//ATTENTION!!! mui.screen instead of mui.viewport
 		return false;
 	});
 
-	$("#tabbar-button1").click(function() {
-		mui.alert("tab 1","Selected");
+	$("#tabbar-button1").click(function () {
+		mui.alert("tab 1", "Selected");
 		return false;
 	});
-	
-	$("#tabbar-button2").click(function() {
-		mui.alert("tab 1","Selected");
+
+	$("#tabbar-button2").click(function () {
+		mui.alert("tab 1", "Selected");
 		return false;
 	});
-	
-	$("#tabbar-button3").click(function() {
-		mui.alert("tab 3","Selected");
+
+	$("#tabbar-button3").click(function () {
+		mui.alert("tab 3", "Selected");
 		return false;
 	});
-	
-	$("#tabbar-button4").click(function() {
-		mui.alert("tab 4","Selected");
+
+	$("#tabbar-button4").click(function () {
+		mui.alert("tab 4", "Selected");
 		return false;
 	});
-	
-	$("#tabbar-button5").click(function() {
-		mui.alert("tab 5","Selected");
+
+	$("#tabbar-button5").click(function () {
+		mui.alert("tab 5", "Selected");
 		return false;
 	});
-	
-	$("#menuoptions").click(function() {
+
+	$("#menuoptions").click(function () {
 		return false;
 	});
-	
+
 	/*******************************************************************************/
 	/*Swipe Test --------------------------------------------------------------------*/
-	/*******************************************************************************/	
+	/*******************************************************************************/
 	//Swipe touch events. Cool for best App user experience!
 	//Evento de desplazamiento tactil. Buenisimo para una óptima experiencia de usuario en App!
-	mui.viewport.on("swiperight", function(currentPageId, originalTarget, event, startX, startY, endX, endY) {
+	mui.viewport.on("swiperight", function (currentPageId, originalTarget, event, startX, startY, endX, endY) {
 		if (!mui.viewport.panelIsOpen()) {
 			mui.history.back();
 		}

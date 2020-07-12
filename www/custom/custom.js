8bb651d30a2e21c8baf39db75e18ff86ba30ce02
acostaNavarroRodrigo <acostanavarro1@gmail.com>
2020-07-12 01:31:38 -0300

    camara prueba 8

--- www/custom/custom.js
+++ www/custom/custom.js
@@ -58,7 +58,7 @@ function prepareCamera() {
         }
     })
     */
-   
+   /* galeria   
    let form = document.getElementById('form');
    //get the captured media file
    let input = document.getElementById('camera');
@@ -69,7 +69,27 @@ function prepareCamera() {
            let img = document.getElementById('imagen');
            img.src = window.URL.createObjectURL(input.files[0]);
        }
-    });
+    });*/
+
+    var player = document.getElementById('player'); 
+    var snapshotCanvas = document.getElementById('snapshot');
+    var captureButton = document.getElementById('capture');
+
+    var handleSuccess = function(stream) {
+        // Attach the video stream to the video element and autoplay.
+        player.srcObject = stream;
+      };
+    
+      captureButton.addEventListener('click', function() {
+        var context = snapshot.getContext('2d');
+        // Draw the video frame to the canvas.
+        context.drawImage(player, 0, 0, snapshotCanvas.width, 
+            snapshotCanvas.height);
+      });
+    
+      navigator.mediaDevices.getUserMedia({video: true})
+          .then(handleSuccess);
+
     // Access the device camera and stream to cameraView
     
    // function cameraStart() {
